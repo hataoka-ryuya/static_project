05f1f7c879b20047ac3f996a651a7d5d7b6d2b73
畠岡龍也 <work.hataoka3@gmail.com>
2023-08-28 22:38:57 +0900

    commit.

--- src/main/java/practice/Practice.java
+++ src/main/java/practice/Practice.java
@@ -3,12 +3,12 @@ package practice;
 public class Practice {
 
 	public static void main(String[] args) {
-//		PracticeWhile.practiceWhile();
-//		PracticeDoWhile.practiceDoWhile();
-//		PracticeFor.practiceFor();
-//		PracticeForEach.practiceForEach();
+		PracticeWhile.practiceWhile();
+		PracticeDoWhile.practiceDoWhile();
+		PracticeFor.practiceFor();
+		PracticeForEach.practiceForEach();
 		PracticeIf.practiceIf();
-//		PracticeSwitch.practiceSwitch();
+		PracticeSwitch.practiceSwitch();
 	}
 
 }
--- src/main/java/practice/PracticeFor.java
+++ src/main/java/practice/PracticeFor.java
@@ -3,7 +3,7 @@ package practice;
 public class PracticeFor {
 	public static void practiceFor() {
 		int sum = 0;
-		
+
 		for (int i = 1; i <= 100; i++) {
 			sum += i;
 		}
